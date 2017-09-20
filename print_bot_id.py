36eb29ffa1dec138089e365129d480f2121e91ca
Lai <laialexanderholmsen@gmail.com>
2017-09-20 20:14:32 +0200

    added script to get bot it

---
+++ print_bot_id.py
@@ -0,0 +1,17 @@
+import os
+from slackclient import SlackClient
+
+BOT_NAME = 'esnbot'
+
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+if __name__ == '__main__':
+    api_call = slack_client.api_call('users.list')
+    if api_call.get('ok'):
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == BOT_NAME:
+                print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+    else:
+        print("Could not find bot user with the name " + BOT_NAME)
+        
