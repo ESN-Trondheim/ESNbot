8fb26b9123c747c829886eb60421b4fe3450282d
Lai <laialexanderholmsen@gmail.com>
2017-09-29 11:12:55 +0200

    added main method

--- esnbot.py
+++ esnbot.py
@@ -7,3 +7,13 @@ BOT_ID = os.environ.get("BOT_ID")
 
 # Constants
 AT_BOT = "<@" + BOT_ID + ">"
+
+# Instantiate Slack client
+slack_client = SlackClient(os.environ.get("SLACK_BOT_TOKEN")) # pylint: disable=invalid-name
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DEALY = 1
+    if slack_client.rtm_connect():
+        print("esnbot connected and running.")
+    else:
+        print("Connection failed.")
