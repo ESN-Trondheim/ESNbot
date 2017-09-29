3dced67b8d65d290f2b76531b7ab7b67325a313a
Lai <laialexanderholmsen@gmail.com>
2017-09-29 22:58:14 +0200

    implemented some commands. you can now tag the bot and enter a command, and the bot will answer.

--- esnbot.py
+++ esnbot.py
@@ -18,38 +18,118 @@ slack_client = SlackClient(os.environ.get("SLACK_BOT_TOKEN")) #pylint: disable=i
 
 def parse_slack_output(slack_rtm_output):
     """
-        This function parses messages written in channels that the bot is a part of.
-        If the message is directed at the bot, this function returns the message
-        (without the mention), the channel ID and the user ID.
-        Returns None if the message is not directed at the bot.
+    Parses messages written in channels that the bot is a part of.
+
+    :Args:
+    `slack_rtm_output` is the events that are fired in the channel
+
+    :Returns:
+    `text`, `channel`, `user` where text is the message after the bot is mentioned,
+    channel is the id of the channel, user is the id of the user.
+    If this information is not present, None, None, None is returned.
     """
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
             print(output, flush=True)
             if output and 'text' in output and AT_BOT in output['text']:
-                return output['text'].split(AT_BOT)[1].strip().lower(), output['channel'], output['user']
+                return (output['text'].split(AT_BOT)[1].strip().lower(), output['channel'],
+                        output['user'])
     return None, None, None
 
 def mention_user(user):
     """
-        Parameter is the user ID. Returns a string that mentiosn the user.
+    Helper function to make it easier to mention users
+
+    :Args:
+    `user` is the ID of the user
+
+    :Returns:
+    A string that mentions the user
     """
     return "<@" + user + ">"
 
 def handle_command(command, channel, user):
     """
-        temp
+    Handles a command directed at the bot
+
+    :Args:
+    `command` is the command the user entered
+
+    `channel` is the ID of the channel the message was written in
+
+    `user` is the ID of the user that originally wrote the message
+
+    :Returns:
+    Nothing
     """
-    print(command, flush=True)
-    slack_client.api_call("chat.postMessage", channel=channel, text="You tagged me, " + mention_user(user), as_user=True)
+    print("Command used was '" + command + "'", flush=True)
+    commands = [
+        "help",
+        "list",
+        "kontaktinfo",
+        "ølstraff",
+        "vinstraff",
+        "reimbursement"
+    ]
+    if command not in commands:
+        slack_client.api_call("chat.postEphemeral", channel=channel, user=user, as_user=True,
+                              text=mention_user(user)
+                              + ", I'm sorry, I dont understand."
+                              + "\nTry '" + AT_BOT + " list' for a list of available commands",)
+    else:
+        choose_command(command, channel, user)
+
+def choose_command(command, channel, user):
+    """
+    Helper function to choose the command corresponding to the command
+    """
+    switcher = {
+        "help": command_help,
+        "list": command_list,
+        "kontaktinfo": command_contact_info,
+        "ølstraff": command_beer_wine_penalty,
+        "vinstraff": command_beer_wine_penalty,
+        "reimbursement": command_reimbursement
+    }
+    func = switcher.get(command)
+    return func(channel, user)
+
+def command_help(channel, user):
+    slack_client.api_call("chat.postMessage", channel=channel, as_user=True,
+                          text=mention_user(user)
+                          + " I'm sorry, this isn't implemented yet. It will be soon™.")
+
+def command_list(channel, user):
+    slack_client.api_call("chat.postMessage", channel=channel, as_user=True,
+                          text=mention_user(user) + "\nAvailable commands are:\n"
+                          + "`list`\n"
+                          + "`help`\n"
+                          + "`kontaktinfo`\n"
+                          + "`ølstraff`\n"
+                          + "`vinstraff`\n"
+                          + "`reimbursement`")
+
+def command_contact_info(channel, user):
+    slack_client.api_call("chat.postMessage", channel=channel, as_user=True,
+                          text=mention_user(user) + "\n" + os.environ.get("CONTACT_INFO"))
+
+def command_beer_wine_penalty(channel, user):
+    slack_client.api_call("chat.postMessage", channel=channel, as_user=True,
+                          text=mention_user(user)
+                          + " I'm sorry, this isn't implemented yet. It will be soon™.")
+
+def command_reimbursement(channel, user):
+    slack_client.api_call("chat.postMessage", channel=channel, as_user=True,
+                          text=mention_user(user) + "\nReimbursement form: " + os.environ.get("REIMBURSEMENT_FORM")
+                          + "\nGuidelines: " + os.environ.get("REIMBURSEMENT_FORM_GUIDELINES"))
 
 def run():
     """
         Main function
     """
     if slack_client.rtm_connect():
-        print("esnbot connected and running.", flush=True)
+        print("ESNbot connected and running.", flush=True)
         while True:
             command, channel, user = parse_slack_output(slack_client.rtm_read())
             if command and channel:
@@ -60,4 +140,3 @@ def run():
 
 if __name__ == "__main__":
     run()
-    
