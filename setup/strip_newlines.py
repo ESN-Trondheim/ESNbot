7d608273f95554cc1cfbadb297ff873774786b77
Lai <laialexanderholmsen@gmail.com>
2019-05-12 13:04:12 +0200

    Remove comments

--- setup/strip_newlines.py
+++ setup/strip_newlines.py
@@ -9,27 +9,6 @@ import sys
 def main(argv):
     if len(argv) > 1:
         with open(argv[1], "r") as file:
-            # Some alternative ways of doing this
-
-            # read = file.readlines()
-            # print(read)
-            # read_string = ("").join(read)
-            # stripped_string = read_string.replace("\n", " ")
-            # print(stripped_string)
-
-            # stripped_lines = []
-            # for line in file:
-            #     stripped_lines.append(line.replace("\n", " "))
-            # stripped_string = ("").join(stripped_lines)
-            # print(stripped_string)
-
-            # lines = file.readlines()
-            # stripped_lines = []
-            # for line in lines:
-            #     stripped_lines.append(line.replace("\n", " "))
-            #     # line = line.rstrip()
-            # stripped_string = ("").join(stripped_lines)
-
             read = file.read()
             stripped_string = read.replace("\n", " ")
             with open(argv[1], "w") as file_out:
