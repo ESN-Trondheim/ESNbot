5118a2fd77b4bdc1345a1951c0135173a0c4a132
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2023-10-19 13:00:27 +0200

    Add explanation to the unexplainable

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,5 +1,8 @@
 import pkgutil
 
+# This will dynamically import all modules in this directory
+# which makes them accessible via the commands package, f.ex:
+# import commands.help
 __all__ = []
 for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
     __all__.append(module_name)
