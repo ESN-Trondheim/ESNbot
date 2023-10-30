bdb5a45021c27cc70e5755d490420467a59e12ab
Lai Alexander Holmsen <laialexanderholmsen@gmail.com>
2023-10-30 15:13:50 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,5 +1,13 @@
 cd /opt/ESNbot
+# Not strictly necessary to stop script and deactivate env if screen session is killed anyway
+# "^C" is ctrl+c
+# "^M" is enter, necessary so that commands will run instead of just writing to console.
+# screen -S esnbot stuff "^C" #ctrl+c to stop python script
+# screen -S esnbot stuff "deactivate^M"
 screen -XS esnbot quit
 git pull
-screen -dmS esnbot python3 esnbot/main.py
+screen -dmS esnbot
+screen -S esnbot -X stuff ". env/bin/activate^M"
+screen -S esnbot -X stuff "pip install -r requirements.txt^M"
+screen -S esnbot -X stuff "python3 esnbot/main.py^M"
 screen -S esnbot -X multiuser on
